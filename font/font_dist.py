595829315afed218b6b830ec5b43a49222339ea2
KarlKKang <karl_kai.kang@icloud.com>
2022-01-09 12:04:35 -0500

    Some more font optimizations

--- font/font_dist.py
+++ font/font_dist.py
@@ -8,19 +8,25 @@ font_family = 'Noto Sans JP'
 font_weights = ['300', '400', '500']
 font_display = 'swap'
 
+dir_surfix = '_v2'
+
 is_cjk = True
+
+split_blocks = {
+    'CJK Unified Ideographs': 50, #SC - 70, TC & JP - 50
+    'CJK Unified Ideographs Extension B': 2, #TC - 10, JP - 2
+    #'CJK Unified Ideographs Extension A': 30, #SC - 30, TC - 2
+}
 
 unicode_blocks = {
     'Basic Latin': ['0000','007F'],
     'Latin-1 Supplement': ['0080', '00FF'],
     'Latin Extended-A': ['0100', '017F'],
     'Latin Extended-B': ['0180', '024F'],
-    'IPA Extensions': ['0250', '02AF'],
+    'IPA Extensions': ['0250', '02AF'], 
     'Spacing Modifier Letters': ['02B0', '02FF'],
     'Combining Diacritical Marks': ['0300', '036F'],
     'Greek and Coptic': ['0370', '03FF'],
-    'Phonetic Extensions': ['1D00', '1D7F'],
-    'Phonetic Extensions Supplement': ['1D80', '1DBF'],
     'Greek Extended': ['1F00', '1FFF'],
     'General Punctuation': ['2000', '206F'],
     'Letterlike Symbols': ['2100', '214F'],
@@ -38,8 +44,7 @@ unicode_blocks_cjk = {
     'Kangxi Radicals': ['2F00', '2FDF'],
     'Ideographic Description Characters': ['2FF0', '2FFF'],
     'CJK Symbols and Punctuation': ['3000', '303F'],
-    'Hiragana': ['3040', '309F'],
-    'Katakana': ['30A0', '30FF'],
+    'Hiragana and Katakana': ['3040', '30FF'], #combined block
     'Bopomofo': ['3100', '312F'],
     'Kanbun': ['3190', '319F'],
     'Bopomofo Extended': ['31A0', '31BF'],
@@ -48,17 +53,7 @@ unicode_blocks_cjk = {
     'Enclosed CJK Letters and Months': ['3200', '32FF'],
     'CJK Compatibility': ['3300', '33FF'],
     'CJK Unified Ideographs Extension A': ['3400', '4DBF'],
-    'CJK Unified Ideographs A': ['4E00', '55CF'],
-    'CJK Unified Ideographs B': ['55D0', '5D9F'],
-    'CJK Unified Ideographs C': ['5DA0', '656F'],
-    'CJK Unified Ideographs D': ['6570', '6D3F'],
-    'CJK Unified Ideographs E': ['6D40', '750F'],
-    'CJK Unified Ideographs F': ['7510', '7CDF'],
-    'CJK Unified Ideographs G': ['7CE0', '84AF'],
-    'CJK Unified Ideographs H': ['84B0', '8C7F'],
-    'CJK Unified Ideographs I': ['8C80', '944F'],
-    'CJK Unified Ideographs J': ['9450', '9C1F'],
-    'CJK Unified Ideographs K': ['9C20', '9FFF'],
+    'CJK Unified Ideographs': ['4E00', '9FFF'],
     'CJK Compatibility Ideographs': ['F900', 'FAFF'],
     'Vertical Forms': ['FE10', 'FE1F'],
     'CJK Compatibility Forms': ['FE30', 'FE4F'],
@@ -75,19 +70,48 @@ unicode_blocks_cjk = {
 
 options='--recommended-glyphs --name-IDs="*" --name-legacy'
 
+def split (glyph, count):
+    block = unicode_blocks[glyph]
+    start = int(block[0], 16)
+    end = int(block[1], 16)
+
+    del unicode_blocks[glyph]
+
+    current_count = 0
+    while current_count != count:
+        block_size = round((end - start + 1) / (count - current_count))
+        if block_size < 1:
+            raise Exception('Block size less than 1')
+        current_count += 1
+        unicode_blocks[glyph + ' part' + str(current_count)] = ['{0:0{1}X}'.format(start, 4), '{0:0{1}X}'.format(start + block_size - 1, 4)]
+        start = start + block_size
+
+if is_cjk:
+    unicode_blocks.update(unicode_blocks_cjk)
+    
+#split
+temp_keys = []
+for glyph in unicode_blocks:
+    temp_keys.append(glyph)
+for glyph in temp_keys:
+    if glyph in split_blocks:
+        split (glyph, split_blocks[glyph])
+
 for i in range(len(font_names)):
     font_name = font_names[i]
-    if not os.path.exists(font_name):
-        os.makedirs(font_name)
+    dir_name = font_name + dir_surfix
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
     css = open(font_name + ".css", "w")
     css.write('@charset "UTF-8";\n/* CSS Document */\n\n')
-    if is_cjk:
-        unicode_blocks.update(unicode_blocks_cjk)
+
+    #compress
     for glyph in unicode_blocks:
-        subprocess.run('pyftsubset "'+font_name+'.'+font_extension+'" --unicodes="U+'+unicode_blocks[glyph][0]+'-'+unicode_blocks[glyph][1]+'" ' + options + ' --flavor="woff2" --output-file="'+font_name+'/'+font_name+'_'+unicode_blocks[glyph][0]+'.woff2"', shell=True, check=True)
-        subprocess.run('pyftsubset "'+font_name+'.'+font_extension+'" --unicodes="U+'+unicode_blocks[glyph][0]+'-'+unicode_blocks[glyph][1]+'" ' + options + ' --flavor="woff" --output-file="'+font_name+'/'+font_name+'_'+unicode_blocks[glyph][0]+'.woff"', shell=True, check=True)
-        css.write('/*'+glyph+'*/\n@font-face {\nfont-family: "'+font_family+'";\nsrc: url("'+font_name+'/'+font_name+'_'+unicode_blocks[glyph][0]+'.woff2") format("woff2"),\nurl("'+font_name+'/'+font_name+'_'+unicode_blocks[glyph][0]+'.woff") format("woff");\nfont-weight: '+font_weights[i]+';\nfont-display: '+font_display+';\nunicode-range: U+'+unicode_blocks[glyph][0]+'-'+unicode_blocks[glyph][1]+';\n}\n\n')
+        command = 'pyftsubset "'+font_name+'.'+font_extension+'" --unicodes="U+'+unicode_blocks[glyph][0]+'-'+unicode_blocks[glyph][1]+'" ' + options
+        file_name = font_name+'_'+unicode_blocks[glyph][0]
+        subprocess.run(command + ' --flavor="woff2" --output-file="'+dir_name+'/'+file_name+'.woff2"', shell=True, check=True)
+        subprocess.run(command + ' --flavor="woff" --output-file="'+dir_name+'/'+file_name+'.woff"', shell=True, check=True)
+        css.write('/*'+glyph+'*/\n@font-face {\nfont-family: "'+font_family+'";\nsrc: url("'+dir_name+'/'+file_name+'.woff2") format("woff2"),\nurl("'+dir_name+'/'+file_name+'.woff") format("woff");\nfont-weight: '+font_weights[i]+';\nfont-display: '+font_display+';\nunicode-range: U+'+unicode_blocks[glyph][0]+'-'+unicode_blocks[glyph][1]+';\n}\n\n')
     css.close()
 
 
-    
