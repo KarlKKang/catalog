d50a41f91794bfd15eee03d5a1dc91b2e05466e2
KarlKKang <karl_kai.kang@icloud.com>
2022-11-19 04:57:34 -0500

    Avoid `window` become `never` in typescript 4.9

--- src/script/browser.ts
+++ src/script/browser.ts
@@ -1,49 +1,50 @@
 (function () {
     const URL = 'https://featherine.com/unsupported_browser';
+    const windowLocation = window.location;
 
     if (!('onerror' in window)) {
-        window.location.replace(URL);
+        windowLocation.replace(URL);
         return;
     }
 
     if (!window.addEventListener) {
-        window.location.replace(URL);
+        windowLocation.replace(URL);
         return;
     }
 
     if (!window.SyntaxError) {
-        window.location.replace(URL);
+        windowLocation.replace(URL);
         return;
     }
 
     window.addEventListener('error', function (e) {
         if (e.error instanceof SyntaxError) {
-            window.location.replace(URL);
+            windowLocation.replace(URL);
         }
     }, true);
 
     if (!window.Function || !Function.prototype.bind) {
-        window.location.replace(URL);
+        windowLocation.replace(URL);
         return;
     }
 
     if (!window.XMLHttpRequest || !('withCredentials' in new XMLHttpRequest())) {
-        window.location.replace(URL);
+        windowLocation.replace(URL);
         return;
     }
 
     if (!window.HTMLScriptElement) {
-        window.location.replace(URL);
+        windowLocation.replace(URL);
         return;
     }
 
     if (!('noModule' in HTMLScriptElement.prototype)) {
-        window.location.replace(URL);
+        windowLocation.replace(URL);
         return;
     }
 
     if (!window.CSS || !window.CSS.supports || !window.CSS.supports('(--a: 0)')) { // https://github.com/jhildenbiddle/css-vars-ponyfill/blob/master/src/index.js
-        window.location.replace(URL);
+        windowLocation.replace(URL);
         return;
     }
 })();
