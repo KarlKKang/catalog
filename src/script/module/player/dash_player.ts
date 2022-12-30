381eaca2285cd56a8719e3249982f031c8f4c576
KarlKKang <karl_kai.kang@icloud.com>
2022-12-30 16:18:27 -0500

    Add dash player class

---
+++ src/script/module/player/dash_player.ts
@@ -0,0 +1,79 @@
+import { NonNativePlayer } from './non-native-player';
+import { remove } from '../DOM';
+import type dashjs from 'dashjs';
+
+export class DashPlayer extends NonNativePlayer {
+    private readonly dashjsInstance: dashjs.MediaPlayerClass;
+
+    constructor(
+        container: HTMLDivElement,
+        dashjsConstructor: typeof dashjs,
+        dashjsConfig: dashjs.MediaPlayerSettingClass,
+        config?: {
+            audio?: boolean;
+            debug?: boolean;
+        }
+    ) {
+        super(container, config);
+        this.dashjsInstance = dashjsConstructor.MediaPlayer().create();
+        this.dashjsInstance.initialize();
+        this.dashjsInstance.updateSettings(dashjsConfig);
+        this.dashjsInstance.setXHRWithCredentialsForType('InitializationSegment', true);
+        this.dashjsInstance.setXHRWithCredentialsForType('MediaSegment', true);
+    }
+
+    protected attach(this: DashPlayer, onload?: (...args: any[]) => void, onerror?: (...args: any[]) => void): void {
+        this.preattach();
+
+        this.dashjsInstance.on('error', function (this: any, event: dashjs.ErrorEvent) {
+            onerror && onerror.call(this, event);
+        });
+        this.dashjsInstance.on('manifestLoaded', function (this: any, event: dashjs.ManifestLoadedEvent) {
+            onload && onload.call(this, event);
+        });
+        this.dashjsInstance.attachView(this.media);
+        this.media.volume = 1;
+        this.onScreenConsoleOutput('dashjs is attached.');
+    }
+
+    public load(
+        this: DashPlayer,
+        url: string,
+        config?: {
+            play?: boolean | undefined;
+            startTime?: number | undefined;
+            onload?: (...args: any[]) => void;
+            onerror?: (...args: any[]) => void;
+        }
+    ): void {
+        config = config ?? {};
+
+        if (!this.attached) {
+            this.attach(config.onload, config.onerror);
+        }
+
+        const play = config.play === true;
+        const startTime = config.startTime;
+
+        const callback = function (this: DashPlayer) {
+            this.dashjsInstance.off('manifestLoaded', callback);
+            if (startTime !== undefined) {
+                this.seek(startTime);
+            }
+            if (play) {
+                this.play();
+            }
+        }.bind(this);
+
+        this.pause();
+        this.dashjsInstance.on('manifestLoaded', callback);
+        this.dashjsInstance.attachSource(url);
+        this.onScreenConsoleOutput('dashjs source loaded.');
+    }
+
+    public destroy(this: DashPlayer) {
+        this.timer && clearInterval(this.timer);
+        this.dashjsInstance.destroy();
+        remove(this.controls);
+    }
+}
--- src/script/module/player/index.ts
+++ src/script/module/player/index.ts
@@ -1,3 +1,4 @@
 export { Player } from './player';
 export { HlsPlayer } from './hls_player';
 export { VideojsPlayer } from './videojs_player';
+export { DashPlayer } from './dash_player';
