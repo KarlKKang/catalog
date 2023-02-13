2cd26ec6abe325b2630ce1386387f23f655462fb
KarlKKang <karl_kai.kang@icloud.com>
2023-02-12 20:46:12 -0500

    Optimize publish order

--- publish.sh
+++ publish.sh
@@ -5,6 +5,11 @@ if [[ "$#" -ne 2 || ("$1" != "featherine-website" && "$1" != "featherine-website
     exit 1
 fi
 
+echo "Uploading css"
+aws s3 sync "$2" "s3://$1/" --exclude "*" --include "*.css" --no-guess-mime-type --content-type="text/css" --metadata-directive="REPLACE" --delete 2>>"publish.log"
+echo "DONE"
+printf "\n"
+
 echo "Uploading html"
 aws s3 sync "$2" "s3://$1/" --exclude "*" --include "*.html" --no-guess-mime-type --content-type="text/html" --metadata-directive="REPLACE" --delete 2>>"publish.log"
 echo "DONE"
@@ -15,8 +20,8 @@ aws s3 sync "$2" "s3://$1/" --exclude "*" --include "*.js" --no-guess-mime-type 
 echo "DONE"
 printf "\n"
 
-echo "Uploading css"
-aws s3 sync "$2" "s3://$1/" --exclude "*" --include "*.css" --no-guess-mime-type --content-type="text/css" --metadata-directive="REPLACE" --delete 2>>"publish.log"
+echo "Uploading source map"
+aws s3 sync "$2" "s3://$1/" --exclude "*" --include "*.map" --no-guess-mime-type --content-type="application/json" --metadata-directive="REPLACE" --delete 2>>"publish.log"
 echo "DONE"
 printf "\n"
 
@@ -57,10 +62,5 @@ printf "\n"
 
 echo "Uploading site.webmanifest"
 aws s3 sync "$2" "s3://$1/" --exclude "*" --include "*site.webmanifest" --no-guess-mime-type --content-type="application/manifest+json" --metadata-directive="REPLACE" --delete 2>>"publish.log"
-echo "DONE"
-printf "\n"
-
-echo "Uploading source map"
-aws s3 sync "$2" "s3://$1/" --exclude "*" --include "*.map" --no-guess-mime-type --content-type="application/json" --metadata-directive="REPLACE" --delete 2>>"publish.log"
 echo "DONE"
 printf "\n"
