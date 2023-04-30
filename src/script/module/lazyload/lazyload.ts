399d873f01203c2dcd147fee61a90e89cb67f774
KarlKKang <karl_kai.kang@icloud.com>
2023-04-29 22:54:05 -0400

    Optimize lazyload - Use only one instance of IntersectionObserver - Delay loading by 250 ms for fast scolling

--- src/script/module/lazyload/lazyload.ts
+++ src/script/module/lazyload/lazyload.ts
@@ -6,85 +6,99 @@ import {
 } from '../main';
 import {
     getByClass,
-    containsClass,
     addClass,
     getDataAttribute,
+    setDataAttribute,
 } from '../dom';
 import { show as showMessage } from '../message';
 import { invalidResponse } from '../message/template/param/server';
 import * as CDNCredentials from '../type/CDNCredentials';
 import type ImageLoader from '../image_loader';
 
+const observer = new IntersectionObserver(observerCallback, {
+    root: null,
+    rootMargin: '25% 0px 25% 0px',
+    threshold: [0]
+});
+
+const STATUS_ATTR_NAME = 'lazyload-status';
+
 let loader: typeof ImageLoader;
 export default function (imageLoader: typeof ImageLoader) {
     loader = imageLoader;
 
     const elems = getByClass('lazyload');
-    const options = {
-        root: null,
-        rootMargin: '0px 0px 50% 0px',
-        threshold: [0]
-    };
-
     for (const elem of elems) {
-        if (elem instanceof HTMLElement && !containsClass(elem, 'listening')) {
-            const observer = new IntersectionObserver(observerCallback, options);
+        if (getDataAttribute(elem, STATUS_ATTR_NAME) === null) {
             observer.observe(elem);
-            addClass(elem, 'listening');
+            setDataAttribute(elem, STATUS_ATTR_NAME, 'listening');
         }
     }
 }
 
 function observerCallback(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
-    const entry = entries[0];
-    if (entry === undefined) {
-        throw new Error('IntersectionObserverEntry is undefined.');
-    }
-    const target = entry.target;
+    for (const entry of entries) {
+        const target = entry.target;
 
-    if (entry['isIntersecting'] === true) {
-        observer.unobserve(target);
-
-        const src = getDataAttribute(target, 'src');
-        if (src === null) {
-            throw new Error('The "src" attribute is missing on the lazyload element.');
-        }
-
-        const altAttr = getDataAttribute(target, 'alt');
-        const alt = altAttr === null ? src : altAttr;
-
-        const xhrParam = getDataAttribute(target, 'xhr-param');
-        if (xhrParam !== null) {
-            const mediaSessionCredential = getDataAttribute(target, 'media-session-credential');
-            let uri = 'get_image.php';
-            let content = 'p=' + xhrParam;
-            if (mediaSessionCredential === null) {
-                uri = 'get_news_image.php';
-            } else {
-                content = mediaSessionCredential + '&' + content;
-            }
-            sendServerRequest(uri, {
-                callback: function (response: string) {
-                    let credentials: CDNCredentials.CDNCredentials;
-                    try {
-                        credentials = JSON.parse(response);
-                        CDNCredentials.check(credentials);
-                    } catch (e) {
-                        showMessage(invalidResponse);
+        if (entry['isIntersecting']) {
+            if (getDataAttribute(target, STATUS_ATTR_NAME) === 'listening') {
+                setDataAttribute(target, STATUS_ATTR_NAME, 'loading');
+                setTimeout(function () {
+                    if (getDataAttribute(target, STATUS_ATTR_NAME) !== 'loading') {
                         return;
                     }
 
-                    const url = concatenateSignedURL(src, credentials);
-                    loader(target, url, alt, function () {
-                        addClass(target, 'complete');
-                    });
-                },
-                content: content
-            });
+                    observer.unobserve(target);
+
+                    const src = getDataAttribute(target, 'src');
+                    if (src === null) {
+                        throw new Error('The "src" attribute is missing on the lazyload element.');
+                    }
+
+                    const altAttr = getDataAttribute(target, 'alt');
+                    const alt = altAttr === null ? src : altAttr;
+                    const xhrParam = getDataAttribute(target, 'xhr-param');
+                    if (xhrParam !== null) {
+                        const mediaSessionCredential = getDataAttribute(target, 'media-session-credential');
+                        let uri = 'get_image.php';
+                        let content = 'p=' + xhrParam;
+                        if (mediaSessionCredential === null) {
+                            uri = 'get_news_image.php';
+                        } else {
+                            content = mediaSessionCredential + '&' + content;
+                        }
+                        sendServerRequest(uri, {
+                            callback: function (response: string) {
+                                let credentials: CDNCredentials.CDNCredentials;
+                                try {
+                                    credentials = JSON.parse(response);
+                                    CDNCredentials.check(credentials);
+                                } catch (e) {
+                                    showMessage(invalidResponse);
+                                    return;
+                                }
+
+                                const url = concatenateSignedURL(src, credentials);
+                                loader(target, url, alt, function () {
+                                    addClass(target, 'complete');
+                                    setDataAttribute(target, STATUS_ATTR_NAME, 'complete');
+                                });
+                            },
+                            content: content
+                        });
+                    } else {
+                        loader(target, src, alt, function () {
+                            addClass(target, 'complete');
+                            setDataAttribute(target, STATUS_ATTR_NAME, 'complete');
+                        });
+                    }
+                }, 250);
+            }
         } else {
-            loader(target, src, alt, function () {
-                addClass(target, 'complete');
-            });
+            if (getDataAttribute(target, STATUS_ATTR_NAME) === 'loading') {
+                setDataAttribute(target, STATUS_ATTR_NAME, 'listening');
+            }
         }
     }
+
 }
