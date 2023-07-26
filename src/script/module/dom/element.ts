b7f4fad67de9e61af6d6404aaf48bd3af25406e4
KarlKKang <karl_kai.kang@icloud.com>
2023-07-27 02:14:56 +0800

    Move `createSvg` function to `dom` module

--- src/script/module/dom/element.ts
+++ src/script/module/dom/element.ts
@@ -167,6 +167,19 @@ export function createOptionElement() {
     return createElement('option') as HTMLOptionElement;
 }
 
+export function createSVGElement(viewBox: string, path: string): SVGSVGElement {
+    const svg = d.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('viewBox', viewBox);
+    addSVGPath(svg, path);
+    return svg;
+}
+
+function addSVGPath(svg: SVGSVGElement, path: string) {
+    const svgPath = d.createElementNS('http://www.w3.org/2000/svg', 'path');
+    svgPath.setAttribute('d', path);
+    appendChild(svg, svgPath);
+}
+
 export function createTextNode(text: string) {
     return d.createTextNode(text);
 }
