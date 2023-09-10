5ef707bad09285ad18aab44d90320de16cde735e
KarlKKang <karl_kai.kang@icloud.com>
2023-09-10 16:46:31 -0400

    Move `promptForLogin` and `promptForEmailOtp` away from shared modules

--- src/script/my_account/index.ts
+++ src/script/my_account/index.ts
@@ -11,7 +11,8 @@ import {
     disableInput,
     PASSWORD_REGEX,
     EMAIL_REGEX,
-    logout
+    logout,
+    isString
 } from '../module/main';
 import {
     addEventListener,
@@ -31,6 +32,8 @@ import {
     appendChild,
     createButtonElement,
     createAnchorElement,
+    appendChildren,
+    replaceChildren,
 } from '../module/dom';
 import { show as showMessage } from '../module/message';
 import { emailSent as emailSentParam } from '../module/message/template/param';
@@ -62,7 +65,7 @@ import * as TOTPInfo from '../module/type/TOTPInfo';
 import * as RecoveryCodeInfo from '../module/type/RecoveryCodeInfo';
 import isbot from 'isbot';
 import body from './body.html';
-import { initializePopUpWindow, promptForEmailOtp, promptForLogin, promptForTotp } from '../module/pop_up_window';
+import { initializePopUpWindow, promptForTotp } from '../module/pop_up_window';
 import { toCanvas } from 'qrcode';
 
 let currentUsername: string;
@@ -523,7 +526,6 @@ function reauthenticationPrompt(
                 );
             }
         },
-        () => { disableAllInputs(false); },
         message
     );
 }
@@ -558,7 +560,6 @@ function modifyMfaReauthenticationPrompt(
                                 showWarning
                             );
                         },
-                        () => { disableAllInputs(false); },
                         (resetResentTimer, closeWindow) => {
                             sendRequestCallback(
                                 'email=' + emailEncoded + '&password=' + passwordEncoded,
@@ -571,9 +572,226 @@ function modifyMfaReauthenticationPrompt(
                 },
             );
         },
-        () => { disableAllInputs(false); },
         message,
     );
+}
+
+function promptForEmailOtp(
+    submitCallback: (
+        otp: string,
+        closeWindow: () => void,
+        showWarning: () => void,
+    ) => void,
+    resetResendTimerCallback: (
+        resetResendTimer: () => void,
+        closeWindow: () => void,
+    ) => void
+) {
+    initializePopUpWindow().then(function (popUpWindow) {
+        const promptText = createParagraphElement();
+        appendText(promptText, 'メールに送信された認証コードを入力してください。');
+
+        const warningText = createParagraphElement();
+        appendText(warningText, failedTotp);
+        changeColor(warningText, 'red');
+        hideElement(warningText);
+
+        const inputFlexbox = createDivElement();
+        addClass(inputFlexbox, 'input-flexbox');
+
+        const otpInputContainer = createDivElement();
+        addClass(otpInputContainer, 'input-field');
+        const otpInput = createInputElement();
+        otpInput.type = 'text';
+        otpInput.autocomplete = 'one-time-code';
+        otpInput.placeholder = '認証コード';
+        otpInput.maxLength = 6;
+        appendChild(otpInputContainer, otpInput);
+        appendChild(inputFlexbox, otpInputContainer);
+
+        const resendButton = createButtonElement();
+        addClass(resendButton, 'button');
+        const resendButtonText = '再送信する';
+        const resetResendTimer = function () {
+            resendButton.disabled = true;
+            resendButton.style.cursor = 'not-allowed';
+            resendButton.style.width = 'auto';
+            resendButton.innerText = resendButtonText + '（60秒）';
+            let count = 60;
+            const interval = setInterval(function () {
+                count--;
+                if (count <= 0) {
+                    resendButton.disabled = false;
+                    resendButton.style.removeProperty('cursor');
+                    resendButton.style.removeProperty('width');
+                    replaceText(resendButton, resendButtonText);
+                    clearInterval(interval);
+                } else {
+                    replaceText(resendButton, resendButtonText + '（' + count + '秒）');
+                }
+            }, 1000);
+        };
+        resetResendTimer();
+        appendChild(inputFlexbox, resendButton);
+
+        addEventListener(resendButton, 'click', function () {
+            resetResendTimerCallback(
+                resetResendTimer,
+                popUpWindow.hide
+            );
+        });
+
+        const submitButton = createButtonElement();
+        addClass(submitButton, 'button');
+        appendText(submitButton, '送信する');
+        const cancelButton = createButtonElement();
+        addClass(cancelButton, 'button');
+        appendText(cancelButton, 'キャンセル');
+        const buttonFlexbox = createDivElement();
+        addClass(buttonFlexbox, 'input-flexbox');
+        appendChild(buttonFlexbox, submitButton);
+        appendChild(buttonFlexbox, cancelButton);
+
+        const disableAllInputs = function (disabled: boolean) {
+            disableInput(otpInput, disabled);
+            if (resendButton.textContent === resendButtonText) {
+                resendButton.disabled = disabled;
+            }
+            submitButton.disabled = disabled;
+            cancelButton.disabled = disabled;
+
+        };
+        addEventListener(submitButton, 'click', function () {
+            disableAllInputs(true);
+            hideElement(warningText);
+
+            const otp = otpInput.value;
+            if (!/^[2-9A-HJ-NP-Z]{6}$/.test(otp)) {
+                showElement(warningText);
+                return;
+            }
+
+            submitCallback(
+                otp,
+                popUpWindow.hide,
+                function () {
+                    disableAllInputs(false);
+                    showElement(warningText);
+                }
+            );
+        });
+        addEventListener(cancelButton, 'click', function () {
+            disableAllInputs(false);
+            popUpWindow.hide();
+        });
+
+        popUpWindow.show(promptText, warningText, inputFlexbox, buttonFlexbox);
+    });
+}
+
+function promptForLogin(
+    submitCallback: (
+        email: string,
+        password: string,
+        closeWindow: () => void,
+        showWarning: (message: string | Node[]) => void,
+    ) => void,
+    message?: string | Node[]
+) {
+    initializePopUpWindow().then(function (popUpWindow) {
+        const promptText = createParagraphElement();
+        appendText(promptText, 'メールアドレスとパスワードを入力してください。');
+
+        const warningText = createParagraphElement();
+        if (message !== undefined) {
+            if (isString(message)) {
+                appendText(warningText, message as string);
+            } else {
+                appendChildren(warningText, ...message as Node[]);
+            }
+        } else {
+            hideElement(warningText);
+        }
+        changeColor(warningText, 'red');
+
+        const emailInputContainer = createDivElement();
+        addClass(emailInputContainer, 'input-field');
+        addClass(emailInputContainer, 'hcenter');
+        const emailInput = createInputElement();
+        emailInput.type = 'email';
+        emailInput.autocomplete = 'email';
+        emailInput.placeholder = 'メールアドレス';
+        emailInput.autocapitalize = 'off';
+        emailInput.maxLength = 254;
+        appendChild(emailInputContainer, emailInput);
+
+        const passwordInputContainer = createDivElement();
+        addClass(passwordInputContainer, 'input-field');
+        addClass(passwordInputContainer, 'hcenter');
+        const passwordInput = createInputElement();
+        passwordInput.type = 'password';
+        passwordInput.autocomplete = 'current-password';
+        passwordInput.placeholder = 'パスワード';
+        passwordStyling(passwordInput);
+        appendChild(passwordInputContainer, passwordInput);
+
+        const submitButton = createButtonElement();
+        addClass(submitButton, 'button');
+        appendText(submitButton, '送信する');
+        const cancelButton = createButtonElement();
+        addClass(cancelButton, 'button');
+        appendText(cancelButton, 'キャンセル');
+        const buttonFlexbox = createDivElement();
+        addClass(buttonFlexbox, 'input-flexbox');
+        appendChild(buttonFlexbox, submitButton);
+        appendChild(buttonFlexbox, cancelButton);
+
+        const disableAllInputs = function (disabled: boolean) {
+            disableInput(emailInput, disabled);
+            disableInput(passwordInput, disabled);
+            submitButton.disabled = disabled;
+            cancelButton.disabled = disabled;
+
+        };
+        addEventListener(submitButton, 'click', function () {
+            disableAllInputs(true);
+            hideElement(warningText);
+
+            const email = emailInput.value;
+            const password = passwordInput.value;
+
+            if (!EMAIL_REGEX.test(email)) {
+                showElement(warningText);
+                return;
+            }
+
+            if (!PASSWORD_REGEX.test(password)) {
+                showElement(warningText);
+                return;
+            }
+
+            submitCallback(
+                email,
+                password,
+                popUpWindow.hide,
+                function (message: string | Node[]) {
+                    disableAllInputs(false);
+                    if (isString(message)) {
+                        replaceText(warningText, message as string);
+                    } else {
+                        replaceChildren(warningText, ...message as Node[]);
+                    }
+                    showElement(warningText);
+                }
+            );
+        });
+        addEventListener(cancelButton, 'click', function () {
+            disableAllInputs(false);
+            popUpWindow.hide();
+        });
+
+        popUpWindow.show(promptText, warningText, emailInputContainer, passwordInputContainer, buttonFlexbox);
+    });
 }
 
 function promptForTotpSetup(totpInfo: TOTPInfo.TOTPInfo) {
