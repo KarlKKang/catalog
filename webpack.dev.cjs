b22298c7865631ecbfb7e34c65ecd455579746d8
KarlKKang <karl_kai.kang@icloud.com>
2023-10-08 06:09:38 -0400

    Don't use webpack merge to make dev config

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -1,16 +1,10 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.cjs');
+const config = require('./webpack.common.cjs');
 const path = require('path');
 const { addHTMLConfig, addFontLoader, addDefinePlugin } = require('./webpack_helper.cjs');
 
-const config = merge(common, {
-    mode: 'development',
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'dev'),
-    }
-});
-
+config.mode = 'development';
+config.output.path = path.resolve(__dirname, 'dev');
+config.devtool = 'source-map';
 addDefinePlugin(config, true);
 addHTMLConfig(config, true);
 addFontLoader(config, true);
