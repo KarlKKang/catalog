c1eba880584415c942eac8df3ed9bd852f445e15
KarlKKang <karl_kai.kang@icloud.com>
2023-10-17 00:53:35 -0400

    Remove unnecessary loops

--- build_legacy.js
+++ build_legacy.js
@@ -12,51 +12,47 @@ let destDirPrefix = './dist/';
 if (dev) {
     destDirPrefix = './dev/';
 }
-fs.mkdir(destDirPrefix + 'script');
-fs.mkdir(destDirPrefix + 'style');
 
-const jsEntries = ['browser'];
-for (const entry of jsEntries) {
-    const filename = './src/script/' + entry + '.ts';
-    fs.read(filename, function (code) {
-        code = lodashTemplate(code)({ data: { domain: (dev ? 'alpha.' : '') + DOMAIN } });
-        babel.transform(
-            code,
-            { filename: filename, presets: ["@babel/preset-env", "@babel/preset-typescript"] },
-            function (err, result) {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                if (dev) {
-                    fs.write(destDirPrefix + 'script/' + entry + '.js', result.code);
-                    return;
-                }
-                terser(result.code, {
-                    ecma: 5,
-                    parse: {},
-                    compress: {
-                        passes: 5
-                    },
-                    mangle: true, // Note `mangle.properties` is `false` by default.
-                    module: false,
-                    // Deprecated
-                    output: null,
-                    format: null,
-                    //toplevel: false,
-                    nameCache: null,
-                    ie8: true,
-                    keep_classnames: undefined,
-                    keep_fnames: false,
-                    safari10: true,
-                }).then((minified) => {
-                    fs.write(destDirPrefix + 'script/' + entry + '.js', minified.code);
-                }).catch((e) => {
-                    console.error(e);
-                });
+const srcFile = './src/script/browser.ts';
+const destFile = destDirPrefix + 'script/browser.js';
+fs.read(srcFile, function (code) {
+    code = lodashTemplate(code)({ data: { domain: (dev ? 'alpha.' : '') + DOMAIN } });
+    babel.transform(
+        code,
+        { filename: srcFile, presets: ["@babel/preset-env", "@babel/preset-typescript"] },
+        function (err, result) {
+            if (err) {
+                console.error(err);
+                return;
             }
-        );
-    });
-}
+            if (dev) {
+                fs.write(destFile, result.code);
+                return;
+            }
+            terser(result.code, {
+                ecma: 5,
+                parse: {},
+                compress: {
+                    passes: 5
+                },
+                mangle: true, // Note `mangle.properties` is `false` by default.
+                module: false,
+                // Deprecated
+                output: null,
+                format: null,
+                //toplevel: false,
+                nameCache: null,
+                ie8: true,
+                keep_classnames: undefined,
+                keep_fnames: false,
+                safari10: true,
+            }).then((minified) => {
+                fs.write(destFile, minified.code);
+            }).catch((e) => {
+                console.error(e);
+            });
+        }
+    );
+});
 
 cssMinify('./src/css/', destDirPrefix + '/style/', 'unsupported_browser.css');
