ea448b95176e58f1c1d50c0c1c68b618a90250f9
KarlKKang <karl_kai.kang@icloud.com>
2023-11-26 22:13:34 -0500

    Fix popup window import race condition

--- src/script/module/popup_window/index.ts
+++ src/script/module/popup_window/index.ts
@@ -1,3 +1,4 @@
+import { pgid } from '../global';
 import { show as showMessage } from '../message';
 import { moduleImportError } from '../message/template/param';
 
@@ -10,26 +11,32 @@ export async function popupWindowImport() {
     if (popupWindow !== null) {
         return popupWindow.initializePopupWindow;
     }
+    const currentPgid = pgid;
     try {
         popupWindow = await import(
             /* webpackExports: ["initializePopupWindow", "destroy"] */
             './core'
         );
     } catch (e) {
-        showMessage(moduleImportError(e));
+        if (pgid === currentPgid) {
+            showMessage(moduleImportError(e));
+        }
         throw e;
     }
     return popupWindow.initializePopupWindow;
 }
 
 export async function promptForTotpImport() {
+    const currentPgid = pgid;
     try {
         return await import(
             /* webpackExports: ["promptForTotp"] */
             './totp'
         );
     } catch (e) {
-        showMessage(moduleImportError(e));
+        if (pgid === currentPgid) {
+            showMessage(moduleImportError(e));
+        }
         throw e;
     }
 }
