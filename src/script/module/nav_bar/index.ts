a1606b330584b7711fa61b87c83de66611331b36
KarlKKang <karl_kai.kang@icloud.com>
2024-03-23 22:37:57 -0400

    Reuse nav bar import

--- src/script/module/nav_bar/index.ts
+++ src/script/module/nav_bar/index.ts
@@ -2,18 +2,23 @@ import { pgid } from '../global';
 import type { NavBarPage } from './enum';
 import { moduleImportError } from '../message/param';
 import { showMessage } from '../message';
+import { default as NavBarFunc } from './nav_bar';
 
+let navBarFunc: typeof NavBarFunc | null = null;
 export async function addNavBar(page?: NavBarPage, currentPageCallback?: () => void) {
+    if (navBarFunc !== null) {
+        navBarFunc(page, currentPageCallback);
+        return;
+    }
     const currentPgid = pgid;
     try {
-        const module = await import(
-            /* webpackExports: ["default"] */
+        ({ default: navBarFunc } = await import(
             './nav_bar'
-        );
+        ));
         if (currentPgid !== pgid) {
             return;
         }
-        module.default(page, currentPageCallback);
+        navBarFunc(page, currentPageCallback);
     } catch (e) {
         if (currentPgid === pgid) {
             showMessage(moduleImportError(e));
