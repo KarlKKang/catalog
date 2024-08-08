bd5f9d153bcd4089ddce524edb906d00790026b2
KarlKKang <karl_kai.kang@icloud.com>
2024-08-08 13:29:19 +0800

    Abort ongoing XHR when redirecting

--- src/script/module/xhr.ts
+++ src/script/module/xhr.ts
@@ -1,5 +1,7 @@
 import { addEventListener, removeAllEventListeners } from './event_listener';
 
+const allRequests = new Set<XMLHttpRequest>();
+
 export function newXHR(
     url: string,
     method: 'GET' | 'POST',
@@ -7,16 +9,26 @@ export function newXHR(
     callback: () => void,
 ) {
     const xhr = new XMLHttpRequest();
+    allRequests.add(xhr);
     xhr.open(method, url, true);
     xhr.withCredentials = withCredentials;
     addEventListener(xhr, 'load', () => {
         callback();
     });
     addEventListener(xhr, 'loadend', () => {
+        allRequests.delete(xhr);
         removeAllEventListeners(xhr);
     });
     if (method === 'POST') {
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     }
     return xhr;
+}
+
+export function abortAllXhr() {
+    for (const xhr of allRequests) {
+        removeAllEventListeners(xhr);
+        xhr.abort();
+    }
+    allRequests.clear();
 }
