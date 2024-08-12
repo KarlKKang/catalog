6abf2c307043ef4bd17d57393ee30d31e3d40c94
KarlKKang <karl_kai.kang@icloud.com>
2024-08-12 21:10:08 +0800

    Abort xhr only if not already aborted

--- src/script/module/xhr/abort.ts
+++ src/script/module/xhr/abort.ts
@@ -2,7 +2,8 @@ import { removeAllEventListeners } from '../event_listener/remove/all_listeners'
 import { allRequests } from './internal/all_requests';
 
 export function abortXhr(xhr: XMLHttpRequest) {
-    removeAllEventListeners(xhr);
-    xhr.abort();
-    allRequests.delete(xhr);
+    if (allRequests.delete(xhr)) {
+        removeAllEventListeners(xhr);
+        xhr.abort();
+    }
 }
