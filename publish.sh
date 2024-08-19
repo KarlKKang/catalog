d1d9975ac86386f860dc3e0ed22d3dfbff1c67cf
KarlKKang <karl_kai.kang@icloud.com>
2024-08-19 20:10:23 +0800

    Improve upload process to avoid getting an older version of the page

--- publish.sh
+++ publish.sh
@@ -5,23 +5,44 @@ if [[ "$#" -ne 2 || ("$1" != "featherine-website" && "$1" != "featherine-website
     exit 1
 fi
 
-opt='--delete'
 if [[ "$1" == "featherine-website" ]]; then
     opt='--no-overwrite ^(script|style|font)\/ --no-overwrite-exclude ^(script\/browser\.js|style\/unsupported_browser\.css)$'
     node ./aws-s3-js/upload.js "$1" "/" "$2/" --dry-run $opt 2>>"publish.log" || exit 1
+    node ./aws-s3-js/upload.js "$1" "/" "$2/" \
+        --exclude '^(sw\.js|index\.html)$' \
+        --mime '\.css$' 'text/css' \
+        --mime '\.html$' 'text/html' \
+        --mime '\.js$' 'application/javascript;charset=utf-8' \
+        --mime '\.map$' 'application/json' \
+        --mime '\.woff2$' 'font/woff2' \
+        --mime '\.txt$' 'text/plain;charset=utf-8' \
+        --mime '\.xml$' 'text/xml' \
+        --mime '\.ico$' 'image/vnd.microsoft.icon' \
+        --mime '\.svg$' 'image/svg+xml' \
+        --mime '\.png$' 'image/png' \
+        --mime '\.webmanifest$' 'application/manifest+json' \
+        --mime '\.json$' 'application/json' \
+        $opt 2>>"publish.log" || exit 1
+    echo "Waiting 10 seconds for the CloudFront error TTL to expire..."
+    sleep 10
+    node ./aws-s3-js/upload.js "$1" "/index.html" "$2/index.html" --mime '\.html$' 'text/html' 2>>"publish.log" || exit 1
+    echo "Please clear the CloudFront cache for /index.html."
+    read -p "Press Enter to continue..." -r
+    node ./aws-s3-js/upload.js "$1" "/sw.js" "$2/sw.js" --mime '\.js$' 'application/javascript;charset=utf-8' 2>>"publish.log" || exit 1
+    echo "All files have been uploaded. Please clear the CloudFront cache for /sw.js."
+else
+    node ./aws-s3-js/upload.js "$1" "/" "$2/" \
+        --mime '\.css$' 'text/css' \
+        --mime '\.html$' 'text/html' \
+        --mime '\.js$' 'application/javascript;charset=utf-8' \
+        --mime '\.map$' 'application/json' \
+        --mime '\.woff2$' 'font/woff2' \
+        --mime '\.txt$' 'text/plain;charset=utf-8' \
+        --mime '\.xml$' 'text/xml' \
+        --mime '\.ico$' 'image/vnd.microsoft.icon' \
+        --mime '\.svg$' 'image/svg+xml' \
+        --mime '\.png$' 'image/png' \
+        --mime '\.webmanifest$' 'application/manifest+json' \
+        --mime '\.json$' 'application/json' \
+        '--delete' 2>>"publish.log"
 fi
-
-node ./aws-s3-js/upload.js "$1" "/" "$2/" \
-    --mime '\.css$' 'text/css' \
-    --mime '\.html$' 'text/html' \
-    --mime '\.js$' 'application/javascript;charset=utf-8' \
-    --mime '\.map$' 'application/json' \
-    --mime '\.woff2$' 'font/woff2' \
-    --mime '\.txt$' 'text/plain;charset=utf-8' \
-    --mime '\.xml$' 'text/xml' \
-    --mime '\.ico$' 'image/vnd.microsoft.icon' \
-    --mime '\.svg$' 'image/svg+xml' \
-    --mime '\.png$' 'image/png' \
-    --mime '\.webmanifest$' 'application/manifest+json' \
-    --mime '\.json$' 'application/json' \
-    $opt 2>>"publish.log"
