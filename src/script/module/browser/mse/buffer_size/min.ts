1a2be8d78fa95798693e6b58cfb374c2945d2993
KarlKKang <karl_kai.kang@icloud.com>
2024-12-27 20:22:36 -0500

    Increase minimum MSE buffer size to 98MB

--- src/script/module/browser/mse/buffer_size/min.ts
+++ src/script/module/browser/mse/buffer_size/min.ts
@@ -1,1 +1,1 @@
-export const MIN_MSE_BUFFER_SIZE = 95; // The minimum buffer size for MSE in order for the videos to play properly. Browsers with smaller buffer size will be considered as unsupported.
+export const MIN_MSE_BUFFER_SIZE = 98; // The minimum buffer size for MSE in order for the videos to play properly. Browsers with smaller buffer size will be considered as unsupported.
