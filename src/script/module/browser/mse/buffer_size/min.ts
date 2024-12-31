dc1b13d6863ed8b017ff1e1d6f65a79a88456e1d
KarlKKang <karl_kai.kang@icloud.com>
2024-12-30 19:32:08 -0500

    Set the minimum MSE buffer size to 95.5MB

--- src/script/module/browser/mse/buffer_size/min.ts
+++ src/script/module/browser/mse/buffer_size/min.ts
@@ -1,1 +1,3 @@
-export const MIN_MSE_BUFFER_SIZE = 97; // The minimum buffer size for MSE in order for the videos to play properly. Browsers with smaller buffer size will be considered as unsupported.
+// The minimum buffer size for MSE in order for the videos to play properly. Browsers with smaller buffer size will be considered as unsupported.
+// The current minimum is Safari 17 and up on iOS.
+export const MIN_MSE_BUFFER_SIZE = 95.5;
