2934111459bf86202f83a0eb78a404f34230d9c9
KarlKKang <karl_kai.kang@icloud.com>
2025-01-02 16:57:03 -0500

    Use the default go back button text for 400 and 500 responses

--- src/script/module/server/internal/message/status_400_and_500.ts
+++ src/script/module/server/internal/message/status_400_and_500.ts
@@ -3,14 +3,12 @@ import { createParagraphElement } from '../../../dom/element/paragraph/create';
 import { appendText } from '../../../dom/element/text/append';
 import { appendChild } from '../../../dom/node/append_child';
 import { MessageParamKey } from '../../../message/type';
-import { reloadButtonText } from '../../../text/button/reload';
 import { defaultErrorSuffix } from '../../../text/default_error/suffix';
 import { setErrorMessageRedirectUrl } from '../../../message/param/helper/set_error_message_redirect_url';
 
 export function status400And500(responseText: string) {
     const param = {
         [MessageParamKey.MESSAGE]: status400And500Body(responseText),
-        [MessageParamKey.BUTTON]: reloadButtonText,
     };
     setErrorMessageRedirectUrl(param);
     return param;
