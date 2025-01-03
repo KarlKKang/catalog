db05645091199cb728bbe7d93b66ac6513997fd0
KarlKKang <karl_kai.kang@icloud.com>
2025-01-03 13:47:25 -0500

    Rename `baseDomain` to `baseHost` when getting the server and CDN origin

--- src/script/module/env/location/get/origin/cdn.ts
+++ src/script/module/env/location/get/origin/cdn.ts
@@ -2,6 +2,6 @@ import { splitHostname } from '../../internal/split_hostname';
 import { concatenateLocationPrefixToHost } from '../../build/host';
 
 export function getCDNOrigin() {
-    const [locationPrefix, baseDomain] = splitHostname();
-    return 'https://' + concatenateLocationPrefixToHost(locationPrefix, 'cdn.' + baseDomain);
+    const [locationPrefix, baseHost] = splitHostname();
+    return 'https://' + concatenateLocationPrefixToHost(locationPrefix, 'cdn.' + baseHost);
 }
--- src/script/module/env/location/get/origin/server.ts
+++ src/script/module/env/location/get/origin/server.ts
@@ -2,6 +2,6 @@ import { splitHostname } from '../../internal/split_hostname';
 import { concatenateLocationPrefixToHost } from '../../build/host';
 
 export function getServerOrigin(locationPrefixOverride?: string) {
-    const [locationPrefix, baseDomain] = splitHostname();
-    return 'https://' + concatenateLocationPrefixToHost(locationPrefixOverride ?? locationPrefix, 'server.' + baseDomain);
+    const [locationPrefix, baseHost] = splitHostname();
+    return 'https://' + concatenateLocationPrefixToHost(locationPrefixOverride ?? locationPrefix, 'server.' + baseHost);
 }
