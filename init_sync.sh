f7ab61b16db9d71a6dde63fb9427e8c881c9c733
KarlKKang <karl_kai.kang@icloud.com>
2025-06-05 11:16:10 -0400

    mkdir in the init_sync script to avoid incorrect syntax on Windows

--- init_sync.sh
+++ init_sync.sh
@@ -5,5 +5,6 @@ if [[ "$#" -ne 2 || ("$1" != "featherine-website" && "$1" != "featherine-website
     exit 1
 fi
 
+mkdir -p "$2"
 aws s3 sync "s3://$1/" "$2" 2>>"init_sync.log"
 node ./aws-s3-js/verify_dir.js "$1" "/" "$2" 2>>"init_sync.log"
