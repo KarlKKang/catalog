4f83ccd5cf2f79fde0d1014a8966d13d23b2544c
KarlKKang <karl_kai.kang@icloud.com>
2025-06-05 11:34:24 -0400

    Fix init sync fails if directory does not exist

--- init_sync.sh
+++ init_sync.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ "$#" -ne 2 || ("$1" != "featherine-website" && "$1" != "featherine-website-alpha") || ! (-d "$2") ]]; then
+if [[ "$#" -ne 2 || ("$1" != "featherine-website" && "$1" != "featherine-website-alpha") ]]; then
     echo "Usage: $0 S3_BUCKET DIRECTORY" >&2
     exit 1
 fi
 
-mkdir -p "$2"
+mkdir -p "$2" || {
+    echo "Failed to create directory $2" >&2
+    exit 1
+}
 aws s3 sync "s3://$1/" "$2" 2>>"init_sync.log"
 node ./aws-s3-js/verify_dir.js "$1" "/" "$2" 2>>"init_sync.log"
