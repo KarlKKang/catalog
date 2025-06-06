6ec044b8bd09b8859faab70d5aeca93ed6ece55f
KarlKKang <karl_kai.kang@icloud.com>
2025-06-06 10:12:04 -0400

    Consider client version as outdated if the response status is not 200. Also compare the version numbers to ensure it's actually a newer version.

--- src/script/entry/version.ts
+++ src/script/entry/version.ts
@@ -1,4 +1,5 @@
 import { addEventListenerNative } from '../module/event_listener/add/native';
+import { max } from '../module/math';
 import { addTimeoutNative } from '../module/timer/add/native/timeout';
 import { removeTimeoutNative } from '../module/timer/remove/native/timeout';
 
@@ -11,7 +12,7 @@ export function checkClientVersion() {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', '/version', true);
     addEventListenerNative(xhr, 'load', () => {
-        clientVersionOutdated = xhr.responseText !== ENV_CLIENT_VERSION;
+        clientVersionOutdated = xhr.status !== 200 || semverGreater(xhr.responseText, ENV_CLIENT_VERSION);
         scheduleVersionCheck();
     });
     addEventListenerNative(xhr, 'error', () => {
@@ -39,4 +40,17 @@ function clearSchedule() {
         removeTimeoutNative(currentTimeout);
         currentTimeout = null;
     }
+}
+
+function semverGreater(ver: string, refver: string): boolean {
+    if (ver === refver) return false;
+    const verParts = ver.split('.');
+    const refverParts = refver.split('.');
+    for (let i = 0; i < max(verParts.length, refverParts.length); i++) {
+        const verPart = parseInt(verParts[i] ?? '0', 10);
+        const refverPart = parseInt(refverParts[i] ?? '0', 10);
+        if (verPart > refverPart) return true;
+        if (verPart < refverPart) return false;
+    }
+    return false;
 }
