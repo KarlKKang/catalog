68d88396400b16ee49d07ba18f7331498c6a4e9d
KarlKKang <karl_kai.kang@icloud.com>
2026-03-25 10:18:50 +0900

    Disable `@typescript-eslint/no-unsafe-function-type` for `isFunction` helper function

--- src/script/module/type/is/function.ts
+++ src/script/module/type/is/function.ts
@@ -1,3 +1,3 @@
-export function isFunction(val: unknown): val is Function {
+export function isFunction(val: unknown): val is Function { // eslint-disable-line @typescript-eslint/no-unsafe-function-type
     return typeof val === 'function';
 }
