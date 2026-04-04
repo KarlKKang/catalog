47974f4fa20d6d01907f9f561966f9b6884566ee
KarlKKang <karl_kai.kang@icloud.com>
2026-04-04 14:09:52 +0900

    Add a clarification to the comment of `urlencode`

--- src/script/module/string/http_form/urlencode.ts
+++ src/script/module/string/http_form/urlencode.ts
@@ -4,6 +4,8 @@
  * Note that although this function has the same name as PHP's `urlencode`, this function does not percent-encode the characters `! ~ * ' ( )` as PHP's `urlencode` does. Otherwise they are equivalent.
  * This encoding is also the same as used by AWS S3 for encoding object keys.
  *
+ * It's also worth noting that it does not strictly comply with the application/x-www-form-urlencoded specification, which further requires that `! ~ ' ( )` be percent-encoded, but this is a common variation that is widely supported and used in practice.
+ *
  * @param uri A value representing an unencoded component.
  * @return The encoded component, equivalent to the result of JavaScript's built-in encodeURIComponent function, but with spaces replaced by plus signs (+) as required by the application/x-www-form-urlencoded format.
  */
